4795245d9b561321e422f2809b1335b5414f1af0
David Fort <contact@hardening-consulting.com>
2025-03-25 17:58:27 +0100

    bubble: expose some dvcman functions for rdpdr

--- channels/rdpdr/client/devman.h
+++ channels/rdpdr/client/devman.h
@@ -28,7 +28,7 @@
 #include "rdpdr_main.h"
 
 FREERDP_LOCAL void devman_unregister_device(DEVMAN* devman, void* key);
-FREERDP_LOCAL void devman_free(DEVMAN* devman);
+FREERDP_API void devman_free(DEVMAN* devman);
 
 WINPR_ATTR_NODISCARD
 FREERDP_LOCAL UINT devman_load_device_service(DEVMAN* devman, RDPDR_DEVICE* device,
@@ -40,8 +40,8 @@ FREERDP_LOCAL DEVICE* devman_get_device_by_id(DEVMAN* devman, UINT32 id);
 WINPR_ATTR_NODISCARD
 FREERDP_LOCAL DEVICE* devman_get_device_by_type(DEVMAN* devman, UINT32 type);
 
-WINPR_ATTR_MALLOC(devman_free, 1)
+FREERDP_API WINPR_ATTR_MALLOC(devman_free, 1)
 WINPR_ATTR_NODISCARD
-FREERDP_LOCAL DEVMAN* devman_new(rdpdrPlugin* rdpdr);
+FREERDP_API DEVMAN* devman_new(rdpdrPlugin* rdpdr);
 
 #endif /* FREERDP_CHANNEL_RDPDR_CLIENT_DEVMAN_H */
